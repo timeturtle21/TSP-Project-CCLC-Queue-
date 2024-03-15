f502d4ea0e4b8b439586ffa79515f2867be0acb8
Tyler Kalkman <kalkmantyl@gmail.com>
2024-03-15 16:25:40 -0400

    Update LoginPage.js
    Created the login form based off of the  reference image.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,16 @@ const LoginPage = () => {
   return (
     <div>
       <header className='App-header'>
-      <h1>Login Page</h1>
-      {/* Add login form or content here */}
+      <form class='card p-3 bg-light' style={{minWidth:'400px', maxHeight:'340px'}}>
+        <h1 class='p-1'>Coach Login</h1>
+        <div align="left" class="form-group pb-2">
+          <label for="uname" style={{ fontSize: '20px' }}>Username:</label>
+          <input class="form-control"  id="uname" placeholder="Enter your username"></input>
+          <label for="pword" style={{ fontSize: '20px' }}>Password:</label>
+          <input class="form-control" id="pword" type="password" placeholder="Enter your password"></input>
+          <button type="button" class="btn btn-warning btn-block mt-2" style={{ minWidth:'380px'}}>Login</button>
+        </div>
+      </form>
       </header>
     </div>
   );
