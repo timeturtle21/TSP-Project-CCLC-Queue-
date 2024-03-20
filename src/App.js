7b9b6f784bcc14ef377da3fc0a1bd848595d90d7
timeturtle21 <cartersoccer08@gmail.com>
2024-03-20 11:18:00 -0400

    Updated drop down menu choices

--- src/App.js
+++ src/App.js
@@ -35,17 +35,22 @@ function App() {
                             Select Question type 
                         </option> 
                             <option value="1">Programming</option> 
-                            <option value="2">Debugging</option> 
-                            <option value="3">Projects Help</option> 
+                            <option value="2">Theory</option> 
                     </select>
             <label>Relevant Course</label> 
                     <select name="select" id="select"> 
                         <option value="" disabled selected> 
                             Select Relevant Course Number 
                         </option> 
-                            <option value="1">CS 1121</option> 
-                            <option value="2">CS XXXX</option> 
-                            <option value="3">CS XXXX</option> 
+                            <option value="1">CS 1111 (Introduction to Programming in C/C++)</option> 
+                            <option value="2">CS 1121 (Introduction to Programming I)</option> 
+                            <option value="3">CS 1122 (Introduction to Programming II)</option> 
+                            <option value="4">CS 1131 (Accelerated Introduction to Programming)</option> 
+                            <option value="5">CS 1142 (Programming at the HW/SW Interface)</option> 
+                            <option value="6">CS 2311 (Discrete Structures)</option> 
+                            <option value="7">CS 2321 (Data Structures)</option> 
+                            <option value="3">Other</option> 
+
                     </select>                       
             <Button variant="warning">Submit your question</Button>
           </header>
