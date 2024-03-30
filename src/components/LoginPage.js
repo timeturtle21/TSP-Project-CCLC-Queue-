9f15f51384f7e45fd5b8e4ef8dc270e104c6c0fe
kkairaa <xckaira@gmail.com>
2024-03-30 15:19:24 -0400

    test for not permitted mtu emails

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 function AppLogin() {
 const [ user, setUser ] = useState(JSON.parse(localStorage.getItem('user')) || {});
 const [ profile, setProfile ] = useState(JSON.parse(localStorage.getItem('profile')) || null);
-const coaches = ['msmille3@mtu.edu', 'kmstrick@mtu.edu'];
+const coaches = ['msmille3@mtu.edu', 'xckaira@gmail.com'];
 
 const login = useGoogleLogin({
   onSuccess: (codeResponse) => {
