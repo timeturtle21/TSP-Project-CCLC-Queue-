9a007721094a7f605b1b007bf74e139c34a5c72e
timeturtle21 <cartersoccer08@gmail.com>
2024-04-03 11:41:48 -0400

    Update QuestionForm.js

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,51 +1,85 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import React from 'react';
 import './QuestionForm.css';
 
 
 
 const QuestionForm = () => {
+    const [formData, setFormData] = useState({
+        questionText: '',
+        questionType: '',
+        relevantCourse: ''
+    });
 
-   
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
 
-    return(
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { questionText, questionType, relevantCourse } = formData;
+        const newQuestion = { questionText, questionType, relevantCourse };
+        alert('Question submitted successfully!');
+        console.log('Submitted Question:', newQuestion);
+        // You can store the newQuestion in an array or perform any other actions here
+        setFormData({
+            questionText: '',
+            questionType: '',
+            relevantCourse: ''
+        }); // Clear the form inputs after submission
+    };
+
+    return (
         <div>
-            
-            <form onSubmit={() => alert('Submitting!')}>
+            <form onSubmit={handleSubmit}>
                 <h1>Welcome to the CCLC!</h1>
-                <label for="question">Enter Question</label> 
-                    <input 
-                        type="text"
-                        name="questionText"
-                        id="questionText"
-                        placeholder="Enter Question"
-                        />
-                <label>Type of Question</label> 
-                    <select name="select" id="select"> 
-                        <option value="" disabled selected> 
-                            Select Question type 
-                        </option> 
-                            <option value="1">Programming</option> 
-                            <option value="2">Theory</option> 
-                    </select>
-                <label>Relevant Course</label> 
-                    <select name="select" id="select"> 
-                        <option value="" disabled selected> 
-                            Select Relevant Course Number 
-                        </option> 
-                            <option value="1">CS 1111 (Introduction to Programming in C/C++)</option> 
-                            <option value="2">CS 1121 (Introduction to Programming I)</option> 
-                            <option value="3">CS 1122 (Introduction to Programming II)</option> 
-                            <option value="4">CS 1131 (Accelerated Introduction to Programming)</option> 
-                            <option value="5">CS 1142 (Programming at the HW/SW Interface)</option> 
-                            <option value="6">CS 2311 (Discrete Structures)</option> 
-                            <option value="7">CS 2321 (Data Structures)</option> 
-                            <option value="3">Other</option> 
-                    </select>    
-
-                <Button variant="warning">Submit your question</Button>
-                           
+                <label htmlFor="questionText">Enter Question</label>
+                <input
+                    type="text"
+                    name="questionText"
+                    id="questionText"
+                    placeholder="Enter Question"
+                    value={formData.questionText}
+                    onChange={handleChange}
+                />
+                <label htmlFor="questionType">Type of Question</label>
+                <select
+                    name="questionType"
+                    id="questionType"
+                    value={formData.questionType}
+                    onChange={handleChange}
+                >
+                    <option value="" disabled>
+                        Select Question type
+                    </option>
+                    <option value="Programming">Programming</option>
+                    <option value="Theory">Theory</option>
+                </select>
+                <label htmlFor="relevantCourse">Relevant Course</label>
+                <select
+                    name="relevantCourse"
+                    id="relevantCourse"
+                    value={formData.relevantCourse}
+                    onChange={handleChange}
+                >
+                    <option value="" disabled>
+                        Select Relevant Course Number
+                    </option>
+                    <option value="CS 1111 (Introduction to Programming in C/C++)">CS 1111 (Introduction to Programming in C/C++)</option>
+                    <option value="CS 1121 (Introduction to Programming I)">CS 1121 (Introduction to Programming I)</option>
+                    <option value="CS 1122 (Introduction to Programming II)">CS 1122 (Introduction to Programming II)</option>
+                    <option value="CS 1131 (Accelerated Introduction to Programming)">CS 1131 (Accelerated Introduction to Programming)</option>
+                    <option value="CS 1142 (Programming at the HW/SW Interface)">CS 1142 (Programming at the HW/SW Interface)</option>
+                    <option value="CS 2311 (Discrete Structures)">CS 2311 (Discrete Structures)</option>
+                    <option value="CS 2321 (Data Structures)">CS 2321 (Data Structures)</option>
+                    <option value="Other">Other</option>
+                </select>
+                <Button variant="warning" type='submit'>Submit your question</Button>
             </form>
         </div>
     );
