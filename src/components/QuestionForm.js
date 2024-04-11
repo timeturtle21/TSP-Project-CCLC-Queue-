055929e15aef8f4c6f04dd4dd1bbf36170dc86bc
Ash Baird <ambaird@mtu.edu>
2024-04-11 18:07:52 -0400

    Added creation capabilities

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -7,10 +7,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 
-// Airtable.configure({
-//     endpointUrl: 'https://api.airtable.com',
-//     apiKey: 'pataHBiNAHkqRtjq9.8c3789c03d409b5ae01156d584301599ecd3308377132fb4f965a1336cddacb6'
-// });
 var base = new Airtable({apiKey:'pataHBiNAHkqRtjq9.8c3789c03d409b5ae01156d584301599ecd3308377132fb4f965a1336cddacb6'}).base('apptCHdO2VgXDv2wL');
 
 
@@ -32,17 +28,34 @@ const QuestionForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         const { questionText, questionType, relevantCourse } = formData;
-        const newQuestion = { questionText, questionType, relevantCourse };
-        alert('Question submitted successfully!');
-        console.log('Submitted Question:', newQuestion);
-        // You can store the newQuestion in an array or perform any other actions here
+
+        if (!questionText.trim() || !questionType || !relevantCourse) {
+            alert("Please enter a value for every field.");
+            return;
+        }
+        base('CCLCQueue').create([
+            {
+                fields: {
+                    QuestionText: questionText,
+                    QuestionType: questionType,
+                    Course: relevantCourse
+                }
+            }
+        ], function(err) {
+            if (err) {
+                console.error(err);
+                alert('Error submitting question' + err.message);
+                return;
+            }
+            alert('Question submitted successfully!');
+        });
+
         setFormData({
             questionText: '',
             questionType: '',
             relevantCourse: ''
         }); // Clear the form inputs after submission
     };
-
     return (
         <div>
             <form onSubmit={handleSubmit}>
@@ -79,13 +92,15 @@ const QuestionForm = () => {
                     <option value="" disabled>
                         Select Relevant Course Number
                     </option>
-                    <option value="CS 1111 (Introduction to Programming in C/C++)">CS 1111 (Introduction to Programming in C/C++)</option>
-                    <option value="CS 1121 (Introduction to Programming I)">CS 1121 (Introduction to Programming I)</option>
-                    <option value="CS 1122 (Introduction to Programming II)">CS 1122 (Introduction to Programming II)</option>
-                    <option value="CS 1131 (Accelerated Introduction to Programming)">CS 1131 (Accelerated Introduction to Programming)</option>
-                    <option value="CS 1142 (Programming at the HW/SW Interface)">CS 1142 (Programming at the HW/SW Interface)</option>
-                    <option value="CS 2311 (Discrete Structures)">CS 2311 (Discrete Structures)</option>
-                    <option value="CS 2321 (Data Structures)">CS 2321 (Data Structures)</option>
+                    <option value="CS1111">CS1111 (Introduction to Programming in C/C++)</option>
+                    <option value="CS1121">CS1121 (Introduction to Programming I)</option>
+                    <option value="CS1122">CS1122 (Introduction to Programming II)</option>
+                    <option value="CS1131">CS1131 (Accelerated Introduction to Programming)</option>
+                    <option value="DATA1201">DATA1201 (Data Science Programming I)</option>
+                    <option value="DATA1202">DATA1202 (Data Science Programming II)</option>
+                    <option value="CS1142">CS1142 (Programming at the HW/SW Interface)</option>
+                    <option value="CS2311">CS2311 (Discrete Structures)</option>
+                    <option value="CS2321">CS2321 (Data Structures)</option>
                     <option value="Other">Other</option>
                 </select>
                 <Button variant="warning" type='submit'>Submit your question</Button>
